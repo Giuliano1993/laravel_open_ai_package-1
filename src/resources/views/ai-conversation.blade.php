08be873aa148c15de9a07af0a53050eae49d48d5
fbhood <mail@fabiopacifici.com>
2023-03-05 22:29:44 +0100

    improve view components

--- src/resources/views/ai-conversation.blade.php
+++ src/resources/views/ai-conversation.blade.php
@@ -2,21 +2,53 @@
     <div class="row flex-column g-4">
         @forelse( $messages as $message)
         <div class="col">
-            <div class="card {{$message->status == 'sent' ? 'bg-secondary' : 'bg-info'}}">
-                <div class="card-header">
+            <div id="card-message-{{ $message->id }}" class="card {{$message->status == 'sent' ? 'bg-secondary' : 'bg-info'}}" x-data>
+                <div class="card-header d-flex align-items-center justify-content-between">
                     @if($message->status == 'sent')
-                    <div class="avatar d-inline-block bg-dark test-white rounded-circle">
-                        <i class="fas fa-user fa-sm fa-fw"></i>
+                    <div class="avatar">
+                        <span class="username">{{$message->conversation->user->name}}</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-badge" viewBox="0 0 16 16">
+                            <path d="M6.5 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                            <path d="M4.5 0A2.5 2.5 0 0 0 2 2.5V14a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2.5A2.5 2.5 0 0 0 11.5 0h-7zM3 2.5A1.5 1.5 0 0 1 4.5 1h7A1.5 1.5 0 0 1 13 2.5v10.795a4.2 4.2 0 0 0-.776-.492C11.392 12.387 10.063 12 8 12s-3.392.387-4.224.803a4.2 4.2 0 0 0-.776.492V2.5z" />
+                        </svg>
                     </div>
                     @else
                     <div class="ai">
-                        <i class="fas fa-robot fa-sm fa-fw"></i>
+                        <span>Ai-assistant</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-robot" viewBox="0 0 16 16">
+                            <path d="M6 12.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5ZM3 8.062C3 6.76 4.235 5.765 5.53 5.886a26.58 26.58 0 0 0 4.94 0C11.765 5.765 13 6.76 13 8.062v1.157a.933.933 0 0 1-.765.935c-.845.147-2.34.346-4.235.346-1.895 0-3.39-.2-4.235-.346A.933.933 0 0 1 3 9.219V8.062Zm4.542-.827a.25.25 0 0 0-.217.068l-.92.9a24.767 24.767 0 0 1-1.871-.183.25.25 0 0 0-.068.495c.55.076 1.232.149 2.02.193a.25.25 0 0 0 .189-.071l.754-.736.847 1.71a.25.25 0 0 0 .404.062l.932-.97a25.286 25.286 0 0 0 1.922-.188.25.25 0 0 0-.068-.495c-.538.074-1.207.145-1.98.189a.25.25 0 0 0-.166.076l-.754.785-.842-1.7a.25.25 0 0 0-.182-.135Z" />
+                            <path d="M8.5 1.866a1 1 0 1 0-1 0V3h-2A4.5 4.5 0 0 0 1 7.5V8a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1v1a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-1a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1v-.5A4.5 4.5 0 0 0 10.5 3h-2V1.866ZM14 7.5V13a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.5A3.5 3.5 0 0 1 5.5 4h5A3.5 3.5 0 0 1 14 7.5Z" />
+                        </svg>
                     </div>
                     @endif
+                    <div class="meta">
+                        <div class="date">
+                            {{ \Carbon\Carbon::parse($message->created_at)->diffForHumans() }}
+                        </div>
+                    </div>
                 </div>
-                <div class="card-body">
-                    {{ $message->body }}
+
+                <button class="btn btn-sm text-light align-self-end" @click="copyContent({{$message->id}})">
+                    <div class="clipboard_copy text-muted">
+                        <span>{{__('Copy message')}}</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+                            <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+                            <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+                        </svg>
+                    </div>
+                    <div class="clipboard_check d-none">
+                        <span>{{__('Text Copied')}}</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-check-fill" viewBox="0 0 16 16">
+                            <path d="M6.5 0A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3Zm3 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3Z" />
+                            <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1A2.5 2.5 0 0 1 9.5 5h-3A2.5 2.5 0 0 1 4 2.5v-1Zm6.854 7.354-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708.708Z" />
+                        </svg>
+                    </div>
+                </button>
+
+                <div class="card-body" contenteditable="true">
+                    {!! Str::of($message->body)->markdown() !!}
                 </div>
+
             </div>
 
         </div>
@@ -25,5 +57,36 @@
             <p>No messages yet.</p>
         </div>
         @endforelse
+        <script>
+            /* TODO: Refactor this 👇 */
+            function copyContent(id) {
+                // define variables
+                const cardId = "card-message-" + id;
+                // select elements
+                const content = document.querySelector("#" + cardId + " .card-body");
+                const copyIcon = document.querySelector("#" + cardId + " .clipboard_copy");
+                const copyCheckIcon = document.querySelector("#" + cardId + " .clipboard_check");
+
+
+                const range = document.createRange();
+                range.selectNodeContents(content);
+                const selection = window.getSelection();
+                selection.removeAllRanges();
+                selection.addRange(range);
+                document.execCommand("copy");
+
+                // show the copy check icon for 2 seconds then hide it
+                copyCheckIcon.classList.toggle('d-none');
+                copyIcon.style.display = "none";
+
+
+                setTimeout(() => {
+                    copyIcon.style.display = "block";
+                    copyCheckIcon.classList.add('d-none');
+
+                }, 2000);
+
+            }
+        </script>
     </div>
 </div>
--- src/resources/views/ai-prompt.blade.php
+++ src/resources/views/ai-prompt.blade.php
@@ -1,15 +1,34 @@
  <!-- User prompt -->
 
- <div class="prompt-wrapper" style="display: flex;">
-   <textarea form="chat-form" class="form-control" name="prompt" id="prompt" :rows="rows" placeholder="Me: " @keyup.enter="rows++"></textarea>
-   <a class="p-3" href="{{ $url }}" onclick="event.preventDefault(); document.getElementById('chat-form').submit();">
-     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
-       <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
-     </svg>
-   </a>
+ <div class="prompt-wrapper" style="display: flex;" x-data>
+     <textarea oninput="autoResize(this)" form="chat-form" class="form-control rounded-0" name="prompt" id="prompt" placeholder="Me: "></textarea>
+     <a class="p-3 text-light" href="{{ $url }}" onclick="event.preventDefault(); document.getElementById('chat-form').submit();">
+         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+             <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+         </svg>
+     </a>
  </div>
  <form id="chat-form" method="POST" action="{{ $url }}">
-   @csrf
+     @csrf
  </form>
 
  <!-- /Chat Form -->
+ <style>
+     #prompt {
+         resize: none;
+         height: 70px;
+         max-height: 50vh;
+     }
+ </style>
+
+ <script>
+     function autoResize(textarea) {
+         const textAreaHeight = textarea.scrollHeight + 'px';
+         textarea.style.height = 'auto';
+         textarea.style.height = textarea.scrollHeight + 'px';
+         document.querySelector('.conversation').style = {
+             "height": "calc(100%" + " - " + textAreaHeight + ")",
+             "min-height": "50vh"
+         }
+     }
+ </script>
