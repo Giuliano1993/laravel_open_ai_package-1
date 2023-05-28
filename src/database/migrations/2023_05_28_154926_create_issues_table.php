b0c1dfe8fa05c390b296fca1092b257bf027a721
GiulianoGosty <giuliano.gostinfini.93@gmail.com>
2023-05-28 19:45:44 +0200

    [ fix ] creation issue table

--- src/database/migrations/2023_05_28_154926_create_issues_table.php
+++ src/database/migrations/2023_05_28_154926_create_issues_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('issues', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('description');
             $table->enum('provider',['github','gitlab', 'bitbucket']);
             $table->string('url');
             $table->timestamps();
