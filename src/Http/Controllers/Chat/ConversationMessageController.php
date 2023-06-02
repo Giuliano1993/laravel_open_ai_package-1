eb335f4caa789ca9d7ec473fa989eb6af5a9fb1f
fbhood <mail@fabiopacifici.com>
2023-06-03 00:39:12 +0200

    initial refactoring openai class

--- src/Http/Controllers/Chat/ConversationMessageController.php
+++ src/Http/Controllers/Chat/ConversationMessageController.php
@@ -49,12 +49,11 @@ class ConversationMessageController extends Controller
             $isShared = $conversation->user_id !== Auth::id();
 
             $hasWriteAccess = false;
-            if($isShared){
+            if ($isShared) {
                 $sharedRow = $conversation->sharedWithUsers()->where('user_id', Auth::id())->first()->pivot;
                 $hasWriteAccess = $sharedRow->write_access;
             }
-            return view('admin.conversations.show', compact('messages', 'url', 'conversation', 'git_providers', 'isShared','hasWriteAccess'));
-
+            return view('admin.conversations.show', compact('messages', 'url', 'conversation', 'git_providers', 'isShared', 'hasWriteAccess'));
         } else {
             abort('403', 'You can access only your conversations!');
         }
@@ -66,50 +65,109 @@ class ConversationMessageController extends Controller
      *
      * @param  \App\Http\Requests\StoreMessageRequest $request
      * @param  \PacificDev\LaravelOpenAi\Services\OpenAi $ai
-     * @return \Illuminate\Http\RedirectResponse | string
+     *
      */
-    public function store(Conversation $conversation, StoreMessageRequest $request, OpenAi $ai): \Illuminate\Http\RedirectResponse | string
+    public function store(Conversation $conversation, StoreMessageRequest $request, OpenAi $ai)
     {
 
-        $isSharedWithUser = $conversation->sharedWithUsers()->where('user_id',Auth::id())->first()?->pivot;
-        if(($conversation->user_id != Auth::id() && !$isSharedWithUser) || ($conversation->user_id != Auth::id() && !$isSharedWithUser->write_access)){
+        // Check if the auth user can add messages to a shaded conversation
+        //dd($request->all());
+        $isSharedWithUser = $conversation->sharedWithUsers()->where('user_id', Auth::id())->first()?->pivot;
+
+        if (($conversation->user_id != Auth::id() && !$isSharedWithUser) || ($conversation->user_id != Auth::id() && !$isSharedWithUser->write_access)) {
             return redirect()->back()->with('message', 'You can\'t send message in this conversation');
         }
-        Message::create([
-            'body' => $request['prompt'],
-            'status' => 'sent',
-            'conversation_id' => $conversation->id
-        ]);
+        // Store the sent message in the db
+        $this->storeSentMessage($conversation->id, $request['prompt']);
+        // Check if the request has a temperature key and if so store it in the temperature variable.
+        if ($request->has('temperature')) {
+            $temperature = floatval($request->temperature);
+        }
+        // Check if the request has a max_tokens key and if so sets it in the max_tokens variable.
+        if ($request->has('max_tokens')) {
+            $max_tokens = intval($request->max_tokens);
+        }
+
 
         try {
-
-            if ($request->has('temperature')) {
-                $temperature = floatval($request->temperature);
+            // Get the chat model response or catch the exception
+            $content = $request['prompt'];
+            if (is_null($content)) {
+                $messages = config('openai.presets.chat.assistant');
+            } else {
+                $preset = config('openai.presets.chat.assistant');
+                $role = 'user';
+                $messages = [...$preset, compact('role', 'content')];
             }
-            if ($request->has('max_tokens')) {
-                $max_tokens = intval($request->max_tokens);
-            }
-
-            //dd($request['prompt'], $temperature ??= 0, $model ??= 'gpt-3.5-turbo', $max_tokens ??= 1500);
-            $text = $ai->chat($request['prompt'], $temperature ??= 0, $model ??= 'gpt-3.5-turbo', $max_tokens ??= 1500);
-            Message::create([
-                'status' => 'received',
-                'body' => $text,
-                'conversation_id' => $conversation->id
-            ]);
-
-            if ($conversation->messages->count() <= 2) {
-                //$conversation_messages = $conversation->messages->pluck('body')->join('');
-                $title = $ai->text_complete('Generate a title for the new conversation: ', $conversation);
-                $conversation->update(['summary' => $title]);
-            }
-
-            return redirect()->back()->with('chat', $text);
-        } catch (\Throwable $th) {
-            return $th->getMessage();
+            //dd($messages);
+            $payload = [
+                'messages' => $messages,
+                'temperature' => $temperature ??= 0,
+                'model' => $model ??= 'gpt-3.5-turbo',
+                'max_tokens' => $max_tokens ??= 1500
+            ];
+            //dd($payload);
+            $response = $ai->chat($payload);
+        } catch (Exception $error) {
+            //dd($error->getMessage());
+            //return redirect()->back()->with('msssage', $error->getMessage());
+            return $error->getMessage();
         }
+        //dd($answer);
+        //dd($ai->getAnswer($response));
+        // check if the response doesn't contain an aswer and return a json error response
+
+
+        if (!$ai->getAnswer($response)) {
+            //dd('here we are');
+            return [
+                'success' => false,
+                'error' => $ai->getFailureMessage($response)
+            ];
+        }
+        // otherwise get the answer
+        $answer = $ai->getAnswer($response);
+        //dd($answer);
+        //dd($conversation->id);
+        // store the message response
+        $this->storeAiReplyMessage($conversation->id, $answer);
+        //dd('here');
+        //Generates a conversation summary based on the sent message
+        if ($conversation->messages->count() <= 2) {
+            $conversation_messages = $conversation->messages->pluck('body')->join('');
+            $title = $ai->text_complete("Generate a title for the new conversation below:\n", $conversation_messages);
+            $conversation->update(['summary' => $title]);
+        }
+        // redirect back
+        return response()->json([
+            'success' => true,
+            'message' => $answer
+        ]);
     }
 
+
+
+    private function storeSentMessage($conversationId, $body)
+    {
+        //dd($conversationId);
+        Message::create([
+            'body' => $body,
+            'status' => 'sent',
+            'conversation_id' => $conversationId
+        ]);
+    }
+
+    private function storeAiReplyMessage($conversationId, $body)
+    {
+        //dd($conversationId);
+
+        //dd($conversationId, $body);
+        Message::create([
+            'body' => $body,
+            'status' => 'received',
+            'conversation_id' => $conversationId
+        ]);
+    }
     /**
      * Remove the specified resource from storage.
      *
